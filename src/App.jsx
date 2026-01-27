969c8bebcfaf46c1e36a8a8b226e7292b8614715
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-01-27 20:22:11 +0200

    Phase6: src/App.jsx - Add scroll restoration for route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import './App.css'
 import LaunchList from './components/LaunchList'
 import LaunchDetails from './components/LaunchDetails'
+
+function ScrollToTop() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return null;
+}
 
 function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LaunchList />} />
         <Route path="/launch/:id" element={<LaunchDetails />} />
