41e52472fcc15b470ea12f8cafd4bbe4ad0b56c4
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-02-01 00:10:49 +0200

    Remove App.css, use Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
-import './App.css'
 import LaunchList from './components/LaunchList'
 import LaunchDetails from './components/LaunchDetails'
 
