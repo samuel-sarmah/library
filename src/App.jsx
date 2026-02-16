bb3b233b71a61e96d2f31e6225ee2f0ae14bc992
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-02-16 07:49:33 +0200

    Fix ScrollToTop to trigger on route change, add node globals to eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import LaunchList from './components/LaunchList'
 import LaunchDetails from './components/LaunchDetails'
 
 function ScrollToTop() {
+  const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, [pathname]);
 
   return null;
 }
